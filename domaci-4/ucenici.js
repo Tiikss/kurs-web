ed2457a324d5125770f454dc2cd8a64653922663
Tiikss <trakocevic7@gmail.com>
2024-04-10 16:52:00 +0200

    Dodavanje ucenika

---
+++ domaci-4/ucenici.js
@@ -0,0 +1,137 @@
+'use strict'
+
+let ucenici = [
+    {
+      ime: "Ana",
+      prezime: "Kovačević",
+      godina_rodjenja: 2005,
+      pol: "ženski",
+      ocjene: {
+        matematika: 4,
+        maternji_jezik: 5,
+        engleski_jezik: 3,
+        biologija: 4,
+        likovna_kultura: 5
+      }
+    },
+    {
+      ime: "Marko",
+      prezime: "Petrović",
+      godina_rodjenja: 2006,
+      pol: "muški",
+      ocjene: {
+        matematika: 5,
+        maternji_jezik: 4,
+        engleski_jezik: 5,
+        biologija: 3,
+        likovna_kultura: 4
+      }
+    },
+    {
+      ime: "Elena",
+      prezime: "Jovanović",
+      godina_rodjenja: 2005,
+      pol: "ženski",
+      ocjene: {
+        matematika: 3,
+        maternji_jezik: 5,
+        engleski_jezik: 4,
+        biologija: 4,
+        likovna_kultura: 4
+      }
+    },
+    {
+      ime: "Ivan",
+      prezime: "Popović",
+      godina_rodjenja: 2007,
+      pol: "muški",
+      ocjene: {
+        matematika: 5,
+        maternji_jezik: 4,
+        engleski_jezik: 5,
+        biologija: 5,
+        likovna_kultura: 3
+      }
+    },
+    {
+      ime: "Jelena",
+      prezime: "Nikolić",
+      godina_rodjenja: 2006,
+      pol: "ženski",
+      ocjene: {
+        matematika: 4,
+        maternji_jezik: 3,
+        engleski_jezik: 5,
+        biologija: 4,
+        likovna_kultura: 4
+      }
+    },
+    {
+      ime: "Stefan",
+      prezime: "Stojanović",
+      godina_rodjenja: 2007,
+      pol: "muški",
+      ocjene: {
+        matematika: 5,
+        maternji_jezik: 4,
+        engleski_jezik: 5,
+        biologija: 3,
+        likovna_kultura: 5
+      }
+    },
+    {
+      ime: "Milica",
+      prezime: "Simić",
+      godina_rodjenja: 2005,
+      pol: "ženski",
+      ocjene: {
+        matematika: 4,
+        maternji_jezik: 5,
+        engleski_jezik: 4,
+        biologija: 3,
+        likovna_kultura: 4
+      }
+    },
+    {
+      ime: "Nikola",
+      prezime: "Pavlović",
+      godina_rodjenja: 2006,
+      pol: "muški",
+      ocjene: {
+        matematika: 3,
+        maternji_jezik: 4,
+        engleski_jezik: 5,
+        biologija: 5,
+        likovna_kultura: 4
+      }
+    },
+    {
+      ime: "Sara",
+      prezime: "Ilić",
+      godina_rodjenja: 2007,
+      pol: "ženski",
+      ocjene: {
+        matematika: 5,
+        maternji_jezik: 3,
+        engleski_jezik: 5,
+        biologija: 4,
+        likovna_kultura: 3
+      }
+    },
+    {
+      ime: "Luka",
+      prezime: "Đorđević",
+      godina_rodjenja: 2005,
+      pol: "muški",
+      ocjene: {
+        matematika: 4,
+        maternji_jezik: 4,
+        engleski_jezik: 4,
+        biologija: 5,
+        likovna_kultura: 5
+      }
+    }
+  ]
+
+
+  
