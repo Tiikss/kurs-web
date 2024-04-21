77e175873add146302411cbe8b355a3c729686be
Tiikss <trakocevic7@gmail.com>
2024-04-21 22:04:47 +0200

    Izmjena fajlova i implementiranje rjesenja

--- domaci-5/index.js
+++ domaci-5/index.js
@@ -1,97 +1,155 @@
-(function () {
-    const bankOne = [{
-        keyCode: 81,
-        keyTrigger: 'Q',
-        id: 'Heater-1',
-        url: 'https://s3.amazonaws.com/freecodecamp/drums/Heater-1.mp3'
-    }, {
-        keyCode: 87,
-        keyTrigger: 'W',
-        id: 'Heater-2',
-        url: 'https://s3.amazonaws.com/freecodecamp/drums/Heater-2.mp3'
-    }, {
-        keyCode: 69,
-        keyTrigger: 'E',
-        id: 'Heater-3',
-        url: 'https://s3.amazonaws.com/freecodecamp/drums/Heater-3.mp3'
-    }, {
-        keyCode: 65,
-        keyTrigger: 'A',
-        id: 'Heater-4',
-        url: 'https://s3.amazonaws.com/freecodecamp/drums/Heater-4_1.mp3'
-    }, {
-        keyCode: 83,
-        keyTrigger: 'S',
-        id: 'Clap',
-        url: 'https://s3.amazonaws.com/freecodecamp/drums/Heater-6.mp3'
-    }, {
-        keyCode: 68,
-        keyTrigger: 'D',
-        id: 'Open-HH',
-        url: 'https://s3.amazonaws.com/freecodecamp/drums/Dsc_Oh.mp3'
-    }, {
-        keyCode: 90,
-        keyTrigger: 'Z',
-        id: "Kick-n'-Hat",
-        url: 'https://s3.amazonaws.com/freecodecamp/drums/Kick_n_Hat.mp3'
-    }, {
-        keyCode: 88,
-        keyTrigger: 'X',
-        id: 'Kick',
-        url: 'https://s3.amazonaws.com/freecodecamp/drums/RP4_KICK_1.mp3'
-    }, {
-        keyCode: 67,
-        keyTrigger: 'C',
-        id: 'Closed-HH',
-        url: 'https://s3.amazonaws.com/freecodecamp/drums/Cev_H2.mp3'
-    },
-    ];
+const bankOne = [{
+    keyCode: 81,
+    keyTrigger: 'Q',
+    id: 'Heater-1',
+    url: 'https://s3.amazonaws.com/freecodecamp/drums/Heater-1.mp3'
+}, {
+    keyCode: 87,
+    keyTrigger: 'W',
+    id: 'Heater-2',
+    url: 'https://s3.amazonaws.com/freecodecamp/drums/Heater-2.mp3'
+}, {
+    keyCode: 69,
+    keyTrigger: 'E',
+    id: 'Heater-3',
+    url: 'https://s3.amazonaws.com/freecodecamp/drums/Heater-3.mp3'
+}, {
+    keyCode: 65,
+    keyTrigger: 'A',
+    id: 'Heater-4',
+    url: 'https://s3.amazonaws.com/freecodecamp/drums/Heater-4_1.mp3'
+}, {
+    keyCode: 83,
+    keyTrigger: 'S',
+    id: 'Clap',
+    url: 'https://s3.amazonaws.com/freecodecamp/drums/Heater-6.mp3'
+}, {
+    keyCode: 68,
+    keyTrigger: 'D',
+    id: 'Open-HH',
+    url: 'https://s3.amazonaws.com/freecodecamp/drums/Dsc_Oh.mp3'
+}, {
+    keyCode: 90,
+    keyTrigger: 'Z',
+    id: "Kick-n'-Hat",
+    url: 'https://s3.amazonaws.com/freecodecamp/drums/Kick_n_Hat.mp3'
+}, {
+    keyCode: 88,
+    keyTrigger: 'X',
+    id: 'Kick',
+    url: 'https://s3.amazonaws.com/freecodecamp/drums/RP4_KICK_1.mp3'
+}, {
+    keyCode: 67,
+    keyTrigger: 'C',
+    id: 'Closed-HH',
+    url: 'https://s3.amazonaws.com/freecodecamp/drums/Cev_H2.mp3'
+},
+];
 
-    const bankTwo = [{
-        keyCode: 81,
-        keyTrigger: 'Q',
-        id: 'Chord-1',
-        url: 'https://s3.amazonaws.com/freecodecamp/drums/Chord_1.mp3'
-    }, {
-        keyCode: 87,
-        keyTrigger: 'W',
-        id: 'Chord-2',
-        url: 'https://s3.amazonaws.com/freecodecamp/drums/Chord_2.mp3'
-    }, {
-        keyCode: 69,
-        keyTrigger: 'E',
-        id: 'Chord-3',
-        url: 'https://s3.amazonaws.com/freecodecamp/drums/Chord_3.mp3'
-    }, {
-        keyCode: 65,
-        keyTrigger: 'A',
-        id: 'Shaker',
-        url: 'https://s3.amazonaws.com/freecodecamp/drums/Give_us_a_light.mp3'
-    }, {
-        keyCode: 83,
-        keyTrigger: 'S',
-        id: 'Open-HH',
-        url: 'https://s3.amazonaws.com/freecodecamp/drums/Dry_Ohh.mp3'
-    }, {
-        keyCode: 68,
-        keyTrigger: 'D',
-        id: 'Closed-HH',
-        url: 'https://s3.amazonaws.com/freecodecamp/drums/Bld_H1.mp3'
-    }, {
-        keyCode: 90,
-        keyTrigger: 'Z',
-        id: 'Punchy-Kick',
-        url: 'https://s3.amazonaws.com/freecodecamp/drums/punchy_kick_1.mp3'
-    }, {
-        keyCode: 88,
-        keyTrigger: 'X',
-        id: 'Side-Stick',
-        url: 'https://s3.amazonaws.com/freecodecamp/drums/side_stick_1.mp3'
-    }, {
-        keyCode: 67,
-        keyTrigger: 'C',
-        id: 'Snare',
-        url: 'https://s3.amazonaws.com/freecodecamp/drums/Brk_Snr.mp3'
-    }];
-})()
+const bankTwo = [{
+    keyCode: 81,
+    keyTrigger: 'Q',
+    id: 'Chord-1',
+    url: 'https://s3.amazonaws.com/freecodecamp/drums/Chord_1.mp3'
+}, {
+    keyCode: 87,
+    keyTrigger: 'W',
+    id: 'Chord-2',
+    url: 'https://s3.amazonaws.com/freecodecamp/drums/Chord_2.mp3'
+}, {
+    keyCode: 69,
+    keyTrigger: 'E',
+    id: 'Chord-3',
+    url: 'https://s3.amazonaws.com/freecodecamp/drums/Chord_3.mp3'
+}, {
+    keyCode: 65,
+    keyTrigger: 'A',
+    id: 'Shaker',
+    url: 'https://s3.amazonaws.com/freecodecamp/drums/Give_us_a_light.mp3'
+}, {
+    keyCode: 83,
+    keyTrigger: 'S',
+    id: 'Open-HH',
+    url: 'https://s3.amazonaws.com/freecodecamp/drums/Dry_Ohh.mp3'
+}, {
+    keyCode: 68,
+    keyTrigger: 'D',
+    id: 'Closed-HH',
+    url: 'https://s3.amazonaws.com/freecodecamp/drums/Bld_H1.mp3'
+}, {
+    keyCode: 90,
+    keyTrigger: 'Z',
+    id: 'Punchy-Kick',
+    url: 'https://s3.amazonaws.com/freecodecamp/drums/punchy_kick_1.mp3'
+}, {
+    keyCode: 88,
+    keyTrigger: 'X',
+    id: 'Side-Stick',
+    url: 'https://s3.amazonaws.com/freecodecamp/drums/side_stick_1.mp3'
+}, {
+    keyCode: 67,
+    keyTrigger: 'C',
+    id: 'Snare',
+    url: 'https://s3.amazonaws.com/freecodecamp/drums/Brk_Snr.mp3'
+}];
+
+const bankState=document.getElementById('bank-switch');
+const powerState=document.getElementById('power-switch');
+const textField=document.getElementById('text-field');
+const sliderVol=document.querySelector('.slider');
+const volVal=document.getElementById('vol-value');
+
+document.addEventListener('keydown', function(e) {
+    if(!powerState.checked)
+        return;
+
+    const keyCode=e.keyCode;
+
+    function playSound(bank){
+        const soundObject=bank.find((sound)=>sound.keyCode===keyCode);
+
+        if(soundObject){
+            const audio=new Audio(soundObject.url);
+            audio.volume=sliderVol.value*0.01;
+            volVal.innerText=sliderVol.value;
+            audio.play();
+        }
+    }
+
+    if(bankState.checked){
+        playSound(bankOne);
+        textField.innerHTML=bankOne.find((sound)=>sound.keyCode===keyCode).id;
+    }
+    else{
+        playSound(bankTwo);
+        textField.innerHTML=bankTwo.find((sound)=>sound.keyCode===keyCode).id;
+    }
+})
+
+function playClick(element){
+    if(!powerState.checked)
+        return;
+
+    const key=element.innerHTML;
+
+    function playSound(bank){
+        const soundObject=bank.find((sound)=>sound.keyTrigger===key);
+
+        if(soundObject){
+            const audio=new Audio(soundObject.url);
+            audio.volume=sliderVol.value*0.01;
+            volVal.innerText=sliderVol.value;
+            audio.play();
+        }
+    }
+
+    if(bankState.checked){
+        playSound(bankOne);
+        textField.innerHTML=bankOne.find((sound)=>sound.keyTrigger===key).id;
+    }
+    else{
+        playSound(bankTwo);
+        textField.innerHTML=bankTwo.find((sound)=>sound.keyTrigger===key).id;
+    }
+}
 
